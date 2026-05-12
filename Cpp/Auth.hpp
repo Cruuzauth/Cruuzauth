39d553bf536b989dea94d977719b7628b57f6518
Cruuzauth <jonathancruz199712@gmail.com>
2026-05-12 03:48:22 -0300

    Auth.hpp

--- Cpp/Auth.hpp
+++ Cpp/Auth.hpp
@@ -1,28 +1,91 @@
 #pragma once
-#include <string>
+#include <Windows.h>
+#include <iostream>
 #include <vector>
+#include <fstream>
+
+struct channel_struct
+{
+	std::string author;
+	std::string message;
+	std::string timestamp;
+};
 
 namespace CruuzAuth {
-    struct response_data {
-        bool success = false;
-        std::string message;
-        std::string raw;
-    };
+	class api {
+	public:
 
-    class api {
-    public:
-        std::string name, ownerid, secret, version, url;
-        response_data response;
+		std::string name, ownerid, secret, version, url;
 
-        api(std::string name, std::string ownerid, std::string secret, std::string version, std::string url);
+		api(std::string name, std::string ownerid, std::string secret, std::string version, std::string url) : name(name), ownerid(ownerid), secret(secret), version(version), url(url) {}
 
-        void init();
-        void license(std::string key);
-        void login(std::string username, std::string password);
-        void regstr(std::string username, std::string password, std::string key);
-        std::vector<unsigned char> download(std::string fileid);
+		void ban(std::string reason = "");
+		void init();
+		void check();
+		std::vector<unsigned char> download(std::string fileid);
+		void log(std::string msg);
+		void license(std::string key);
+		std::string var(std::string varid);
+		std::string webhook(std::string id, std::string params, std::string body = "", std::string contenttype = "");
+		void setvar(std::string var, std::string vardata);
+		std::string getvar(std::string var);
+		bool checkblack();
+		void web_login();
+		void button(std::string value);
+		void upgrade(std::string username, std::string key);
+		void login(std::string username, std::string password);
+		
+		void regstr(std::string username, std::string password, std::string key, std::string email = "");
+		void chatget(std::string channel);
+		bool chatsend(std::string message, std::string channel);
+		void changeUsername(std::string newusername);
+		std::string fetchonline();
+		void fetchstats();
+		void forgot(std::string username, std::string email);
 
-    private:
-        std::string request(std::string data);
-    };
+		class subscriptions_class {
+		public:
+			std::string name;
+			std::string expiry;
+		};
+
+		class userdata {
+		public:
+
+			std::string username;
+			std::string ip;
+			std::string hwid;
+			std::string createdate;
+			std::string lastlogin;
+
+			std::vector<subscriptions_class> subscriptions;
+		};
+
+		class appdata {
+		public:
+			std::string numUsers;
+			std::string numOnlineUsers;
+			std::string numKeys;
+			std::string version;
+			std::string customerPanelLink;
+		};
+
+		class responsedata {
+		public:
+			std::vector<channel_struct> channeldata;
+			bool success{};
+			std::string message;
+		};
+
+		userdata user_data;
+		appdata app_data;
+		responsedata response;
+		subscriptions_class espira;
+
+	private:
+		std::string sessionid, enckey;
+	};
+
+
+	
 }
